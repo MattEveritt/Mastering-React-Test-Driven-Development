e424cd70f7a14256b0f9b5ee1bb7a0ad9b757554
MattEveritt <60779706+MattEveritt@users.noreply.github.com>
2022-11-07 09:06:17 +0200

    Initial commit

---
+++ appointments/test/Appointment.test.js
@@ -0,0 +1,26 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Appointment } from '../src/Appointment';
+
+describe('Appointment', () => {
+    let container;
+    let customer;
+    beforeEach(() => {
+        container = document.createElement('div');
+    });
+    it('renders the customer first name', () => {
+        customer = { firstName: 'Ashley' };
+        ReactDOM.render(<Appointment customer={customer} />, container);
+        expect(container.textContent).toMatch('Ashley');
+    });
+    it('renders another customer first name', () => {
+        customer = { firstName: 'Jordan' };
+        ReactDOM.render(<Appointment customer={customer} />, container);
+        expect(container.textContent).toMatch('Jordan');
+    });
+});
